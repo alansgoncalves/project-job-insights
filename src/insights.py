08e4a089dee26b2579a9a2d3c5dbaf6c796dead9
Alan Gonçalves <vekio.soft@gmail.com>
2022-03-01 17:52:09 -0300

    requisito 3

--- src/insights.py
+++ src/insights.py
@@ -25,21 +25,8 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    csv_file = read(path)
+    return list({ind["industry"] for ind in csv_file if ind["industry"]})
 
 
 def filter_by_industry(jobs, industry):
